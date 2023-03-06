950df629cb7db3657ae3eb0c20f8010d158ab7a8
Jesulayomy <jesulayomi081@gmail.com>
2023-03-06 05:01:01 +0000

    on time

--- person.py
+++ person.py
@@ -1,15 +1,17 @@
 """ A class that defines a person as an object """
 
-from quirks import *
+import csv
+import json
 import random
 from roles import *
-import json
+from quirks import *
 
 
 class Person:
     """ defines a base class for people """
 
     __population = 0
+    idx = 0
 
     def __init__(self, name):
         """ Initialization """
@@ -17,25 +19,22 @@ class Person:
         if not isinstance(name, str):
             raise TypeError("The name must be a string")
         self.name = name
+        self.role = role()
         Person.__population += 1
 
-        """ sets a role for the person """
-        self.role = role()
-
-        """ assigns a quirk to a user """
         factor = random.randint(0, len(quirk_list))
         self.quirk = quirk_list.pop(factor)
 
     def __str__(self):
         """ introduces a character using the str method"""
 
-        return f"Name: {self.name}\nQuirk: {self.quirk}\nStatus: {self.role}"
+        return f"Name: {self.name}\nQuirk: {self.quirk}\nStatus: {self.role}\n"
 
     @classmethod
-    def population(cls):
+    def population(cls, self):
         """ Prints the total number of people in the session """
 
-        print(f"There are {cls._Person__population} {cls.role} in the world")
+        print(f"There are {cls._Person__population} {self.role} in the world")
 
     @classmethod
     def quirks(cls):
@@ -61,7 +60,10 @@ class Person:
                     pass
         else:
             for key, value in kwargs.items():
-                setattr(self, key, value)
+                if key == 'role':
+                    setattr(self, key, role(value))
+                else:
+                    setattr(self, key, value)
 
     def to_dictionary(self):
         """ Returns the dictionary representation of the instance """
@@ -76,9 +78,114 @@ class Person:
 
     @staticmethod
     def to_json_string(list_dicts):
-        """ returns the json string representation of the dictionaries """
+        """
+            Effectively returns the json string representation of the
+            dictionary of an instance's with def attributes
+        """
 
         if list_dicts is None:
             return "[]"
         else:
             return json.dumps(list_dicts)
+
+    @staticmethod
+    def from_json_string(json_str):
+        """
+            Loads a class from a json string representation
+            It returns a list btw
+        """
+
+        return json.loads(json_str)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """
+            creates an instance using the dictionary
+            always add the ** in the arg, ex: Person.create(**dict1)
+        """
+
+        new = cls("default")
+        new.update(**dictionary)
+        return new
+
+    @classmethod
+    def save_to_file(cls, list_person, filename=None):
+        """ saves a list of people to a file """
+
+        if filename is None:
+            filename = "{}.json".format(cls.__name__)
+        list_dict = []
+        for obj in list_person:
+            list_dict.append(obj.to_dictionary())
+        json_str = cls.to_json_string(list_dict)
+        with open(filename, "a") as f:
+            f.write(json_str)
+
+    @classmethod
+    def load_from_file(cls, from_file=None):
+        """ loads a list of people from a json file """
+
+        if from_file is None:
+            filename = f"{cls.__name__}.json"
+        else:
+            if type(from_file) is not str:
+                raise TypeError("The filename should be a string")
+            filename = from_file
+
+        i = cls.idx
+        list_people = []
+        with open(filename, 'r') as fl:
+            for line in fl:
+                list_dict = cls.from_json_string(line)
+                for j in range(len(list_dict)):
+                    d = list_dict[j]
+                    globals()['bot%s' % i] = cls.create(**d)
+                    list_people += [globals()['bot%s' % i]]
+                    i += 1
+
+        cls.idx = i + 1
+
+        return list_people
+
+    @classmethod
+    def save_to_csv(cls, list_people, filename=None):
+        """ saves to csv """
+
+        if filename is None:
+            filename = f"{cls.__name__}.csv"
+
+        list_keys = ['name', 'quirk', 'role']
+        list_values = ['default', 'None', 'Student']
+        people = []
+        for obj in list_people:
+            for idx in range(len(list_keys)):
+                list_values[idx] = obj.to_dictionary()[list_keys[idx]]
+            people.append(list_values[:])
+
+        with open(filename, 'a') as fl:
+            sheet = csv.writer(fl)
+            sheet.writerows(people)
+
+    @classmethod
+    def load_from_csv(cls, filename=None):
+        """ loads from csv and returns a list """
+
+        if filename is None:
+            filename = f"{cls.__name__}.csv"
+
+        list_keys = ['name', 'quirk', 'role']
+
+        list_people = []
+        dictionary = {}
+        list_dict = []
+        with open(filename, 'r') as fl:
+            sheet = csv.reader(fl)
+            for list_row in sheet:
+                for idx in range(len(list_keys)):
+                    dictionary[list_keys[idx]] = list_row[idx]
+                list_dict.append(dictionary)
+                dictionary = {}
+            for i in range(len(list_dict)):
+                list_people.append(cls.create(**list_dict[i]))
+
+        return list_people
--- tests/test_person.py
+++ tests/test_person.py
@@ -30,3 +30,19 @@ class TestQuirkObjects(unittest.TestCase):
             globals()['u%s' % i] = Person("User{0}".format(i))
 
         self.assertEqual(Person._Person__population, 9)
+
+    def test_3_dicts(self):
+        """ chechs the type of the elements in quirk list """
+
+        for i in range(1, 10):
+            globals()['u%s' % i] = Person("User{0}".format(i))
+
+        d1 = u1.to_dictionary()
+        d2 = u2.to_dictionary()
+        d3 = u3.to_dictionary()
+        d4 = u4.to_dictionary()
+
+        print(d1)
+        print(d2)
+        print(d3)
+        print(d4)
